0d2c698995041dc8a0ad1a1021fd053bda8c2f70
Zaid <zaidoskate@hotmail.com>
2024-07-05 14:33:52 -0600

    Primer commit

---
+++ src/test/java/com/alura/literalura/LiterAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.literalura.LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
